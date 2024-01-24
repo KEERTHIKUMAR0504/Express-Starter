e87f11889741516ae47ed256d56e283b0d8e3d5f
KEERTHIKUMAR0504 <tskeerthikumar0504@gmail.com>
2024-01-24 20:33:59 +0530

    modified user route file and added auth route file

---
+++ routes/products.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const Products = require('../models/products.models')
+
+
+
+router.get('/users',(req,res)=>{});
+router.get('/users',(req,res)=>{});
+router.get('/users',(req,res)=>{});
+router.get('/users',(req,res)=>{});
+
+module.exports = router;
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,36 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const {users} = require('../data');
-const Users = require('../models/users.models')
+const Users = require('../models/users.models');
+const { getAllUsers, getUserById } = require('../controllers/user.controller');
+const { isAuth } = require('../utils/authentication');
 
 
 
-router.get('/users',(req,res)=>{
-    try{
-        Users.find().then((data)=>{
-            res.status(200).send({message:'Users have been retrieved successfully.', data: data})
-        }).catch((error)=>{
-            res.status(400).send({message:"Error while retrieving users."})
-        })
-    }catch(error){
-        res.status(500).send({message:'Internal Server Error.'})
-    }
-});
+router.get('/users',isAuth ,getAllUsers);
 
-router.get('/users/:userId',(req,res)=>{
-    try{
-        Users.findById(req.params.userId).then((data)=>{
-            if(!data){
-            return res.status(200).send({message:'No user found with the given id'})
-            }
-            res.status(200).send({message:'User have been retrieved successfully.', data: data})
-        }).catch((error)=>{
-            res.status(400).send({message:"Error while retrieving user."})
-        })
-    }catch(error){
-        res.status(500).send({message:'Internal Server Error.'})
-    }
-});
+router.get('/users/:userId', isAuth, getUserById);
 
 router.post('/users',(req,res)=>{
     try{
