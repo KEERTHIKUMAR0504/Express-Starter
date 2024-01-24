8a8d7c85a0f05835bd8c59cf4178fcad6c982a35
KEERTHIKUMAR0504 <tskeerthikumar0504@gmail.com>
2024-01-24 20:39:16 +0530

    installed necessary features and created user registration, login, logout and added authentication for users to get users data.

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 require('dotenv').config();
 const express = require('express');
-const userRoutes = require('./routes/users.routes')
+const cookieparser = require('cookie-parser');
+const userRoutes = require('./routes/users.routes');
+const productRoutes = require('./routes/products.routes');
+const authRoutes = require('./routes/auth.routes');
 const app = express();
 const db = require('./db/connect')
 const cors = require('cors')
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieparser());
 
 db();
 
 app.use(userRoutes);
+app.use(productRoutes);
+app.use(authRoutes);
 
 const PORT = process.env.PORT || 8000;
 
